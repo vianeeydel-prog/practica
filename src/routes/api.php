c3cc64e81ccf526a61e0da1aab2b76a8c9aba92b
vianeeydel-prog <vianeeydel@gmail.com>
2026-03-09 23:04:06 +0000

    Practica API eventos funcionando

---
+++ src/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy;']);
+Route::apiResource('eventos', EventoController::class);
